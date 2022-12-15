0ee31a0e6111cf149f58311b3572dfbbd83e4eeb
Ruffin Bailey <git@rufwork.com>
2022-12-14 23:22:16 -0500

    Initial commit. Might work with hashes.

---
+++ HtmRouter.js
@@ -0,0 +1,77 @@
+(function () {
+    window.HtmRouter = function (props) {
+        // =============================================
+        // jive stolen from
+        // https://github.com/preactjs/preact-router
+        // =============================================
+        function prevent(e) {
+            if (e.stopImmediatePropagation) e.stopImmediatePropagation();
+            if (e.stopPropagation) e.stopPropagation();
+            e.preventDefault();
+            return false;
+        }
+
+        // Handles both delegated and direct-bound link clicks
+        function delegateLinkHandler(e) {
+            // ignore events the browser takes care of already:
+            if (e.ctrlKey || e.metaKey || e.altKey || e.shiftKey || e.button) return;
+
+            let t = e.target;
+            do {
+                if (t.localName === "a" && t.getAttribute("href")) {
+                    if (t.hasAttribute("data-native") || t.hasAttribute("native")) return;
+
+                    var newUrl = t.getAttribute("href");
+
+                    // if link is handled by the router, prevent browser defaults
+                    // Some of this stolen stuff was edited. -RUF
+                    // Not real sure what "state" should do here
+                    // https://developer.mozilla.org/en-US/docs/Web/API/History/pushState
+                    // I mean I get it, but what would I put here?
+                    // https://stackoverflow.com/a/34178234/1028230
+                    var match = routerHandlesUrl(newUrl);
+                    if (match) {
+                        setUrl(newUrl);
+                        window.history.pushState(
+                            match.props,
+                            match.props.title || document.title,
+                            newUrl
+                        );
+                        return prevent(e);
+                    }
+                }
+            } while ((t = t.parentNode));
+        }
+
+        let eventListenersInitialized = false;
+
+        function initEventListeners() {
+            if (eventListenersInitialized) return;
+            eventListenersInitialized = true;
+
+            addEventListener("click", delegateLinkHandler);
+        }
+        // =============================================
+        // eo stolen jive
+        // =============================================
+
+        function routerHandlesUrl(url) {
+            return children.find((x) => x.props.path && url.endsWith(x.props.path));
+        }
+
+        const [url, setUrl] = useState(window.location.href);
+        initEventListeners();
+
+        // console.log(props.children, url);
+        var haveChildren = Array.isArray(props.children);
+        var children = haveChildren ? props.children : [];
+
+        var match = haveChildren
+            ? props.children.find((x) => x.props.path && url.endsWith(x.props.path)) ||
+              props.children.find((x) => x.props.default)
+            : html`<h3>no route match</h3>`;
+
+        return match;
+    };
+})();
+
--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # preact-router-for-htm
-Bare bones router for Preact apps using HTM without transpilation
+
+Bare bones router for [Preact](https://preactjs.com) apps using [HTM](https://github.com/developit/htm) (ie, **not** JSX) [without transpilation](https://myfreakinname.blogspot.com/search/label/preact).
+
+Works okay with URLs that change the hash. Less great with changed URLs.
+
+
+### Usage
+
+(This doesn't really count as docs. I'll add some if this works well enough that I make a few more commits.)
+
+Tried to stay kinda close to preact-router. Try throwing something like this in your `index.html` and smoking it:
+
+```
+<script src="https://unpkg.com/preact@10.11.3/dist/preact.umd.js"></script>
+<script src="https://unpkg.com/preact@10.11.3/hooks/dist/hooks.umd.js"></script>
+<script src="https://unpkg.com/htm@3.1.1/preact/standalone.umd.js"></script>
+<script>
+    window.render = window.preact.render;
+    window.useState = window.preactHooks.useState;
+    window.html = window.htmPreact.html;
+
+    document.addEventListener("DOMContentLoaded", function () {
+        render(
+            html`<${HtmRouter}>
+                <${MyComponent} displayText="default" default />
+                <${MyComponent} displayText="simple preact" path="simplePreact/" />
+                <${MyComponent} displayText="another url" path="anotherUrl/" />
+                <${MyComponent} displayText="hash" path="#hash" />
+                <${MyComponent} displayText="another hash" path="#another" />
+            </HtmRouter>`,
+            document.getElementById("root-element")
+        );
+    });
+</script>
+
+<body>
+    <a href="./anotherUrl/">go to url within app</a><br />
+    <a href="#hash">add hash to url</a><br />
+    <a href="#another">add hash to url</a><br />
+    <a href="http://www.apple.com">go to apple.com</a><br />
+    <div id="root-element"></div>
+</body>
+```
+
+Code is really quite short and self-explanatory. Stop reading this and open it up.
+
+Note that it is meant to be run in (and only in) a [non-transpiled Preact & HTM environment](https://myfreakinname.blogspot.com/search/label/preact).
+
+
+### Acknowledgements
+
+Some jive shamelessly stolen from the MIT licensed [preact-router](https://github.com/preactjs/preact-router), which I was debugging with non-transpiled Preact & HTM before I figured it'd be pretty straightforward to write my own -- router, that is.
